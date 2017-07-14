2126ef5b11366dbf7aff720cc7f1d7ca39ca364f
TLs92 <wangxinhan01@qq.com>
2017-07-14 14:59:33 +0800

    mymonitor chgh

--- ryu/app/mymonitor13.py
+++ ryu/app/mymonitor13.py
@@ -1,5 +1,6 @@
+from operator import attrgetter
+
 from ryu.app import simple_switch_13
-from ryu.ofproto import ofproto_v1_3
 from ryu.controller import ofp_event
 from ryu.controller.handler import set_ev_cls
 from ryu.controller.handler import MAIN_DISPATCHER, DEAD_DISPATCHER
@@ -14,8 +15,13 @@ class Mymonitor13(simple_switch_13.SimpleSwitch13):
         self.datapaths = {}
         self.monitor_thread = hub.spawn(self._monitor)
         # a micro thread(coroutine) to run _monitor()
+        # hub.spawn() use eventlet's green process
 
     # get datapath info
+    # EventOFPPortStateChange is a function offered by Ryu structure,when datapath's state changes,
+    # it will be triggered.
+    # datapath's state turn to MAIN_DISPATCHER,means switch is  registered and been watched.
+    # datapath's state turn to DEAD_DISPATCHER,means switch is un-registered.
     @set_ev_cls(ofp_event.EventOFPPortStateChange, [MAIN_DISPATCHER, DEAD_DISPATCHER])
     def _state_chan_handler(self, ev):
         datapath = ev.datapath
@@ -29,11 +35,12 @@ class Mymonitor13(simple_switch_13.SimpleSwitch13):
         elif ev.state == DEAD_DISPATCHER:
             # switch is offline.
             if datapath.id in self.datapaths:
-                # delete is record.
+                # delete this record and log it.
                 del self.datapaths[datapath.id]
                 self.logger.debug("Register datapath: %16x",datapath.id)
 
-    #send request msg periodically.
+    # send request msg periodically.
+    # use eventlet way in __init__ .
     def _monitor(self):
         while True:
             for dp in self.datapaths.values():
@@ -41,17 +48,19 @@ class Mymonitor13(simple_switch_13.SimpleSwitch13):
             hub.sleep(5)
             # 5 seconds intervals for each.
 
-    #send stats request message to datapath.
+    # send stats request message to datapath.
     def _request_stats(self, datapath):
         self.logger.debug("send stats request to datapath: 16%x",datapath.id)
         ofproto = datapath.ofproto
         ofp_parser = datapath.ofp_parser
 
-        #send port stats request msg.
+        # send port stats request msg.
+        # OFPPortStatsRequest requests that the switch provide statistical info related to flow entry.
         req = ofp_parser.OFPPortStatsRequest(datapath)
         datapath.send_msg(req)
 
-        #sned flow stats request msg.
+        # sned flow stats request msg.
+        # OFPFlowStatsRequest requests that the switch provide statistical info related to flow entry.
         req = ofp_parser.OFPFlowStatsRequest(datapath, 0, ofproto.OFPP_ANY)
         datapath.send_msg(req)
 
